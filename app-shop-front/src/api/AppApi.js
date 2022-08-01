90b36e575c8b3f1fbc523eb6dc37779a89cfc140
Men_Kimman <86313031+Hacking1202@users.noreply.github.com>
2022-08-01 16:03:40 +0500

    Update AppApi.js

--- app-shop-front/src/api/AppApi.js
+++ app-shop-front/src/api/AppApi.js
@@ -29,3 +29,48 @@ export const delCategory = (data) => {
     console.log(data)
     return HttpClient.doDelete(api.category + "/" + data.id);
 }
+//Product
+export const getProducts = () => {
+    return HttpClient.doGet(api.product+"/list");
+}
+
+export const addProduct = (data) => {
+    alert("api")
+    return HttpClient.doPost(api.product, data);
+}
+export const updateProduct = (data) => {
+    return HttpClient.doPut(api.product + "/" + data.id, data);
+}
+export const delProduct = (data) => {
+    console.log(data)
+    return HttpClient.doDelete(api.product + "/" + data.id);
+}
+//Detail
+export const getDetails = () => {
+    return HttpClient.doGet(api.detail);
+}
+
+export const addDetail = (data) => {
+    return HttpClient.doPost(api.detail, data);
+
+}
+export const updateDetail = (data) => {
+    return HttpClient.doPut(api.detail + "/" + data.id, data);
+}
+export const delDetail = (data) => {
+    return HttpClient.doDelete(api.detail + "/" + data.id);
+}
+//Measure
+export const getMeasures = () => {
+    return HttpClient.doGet(api.measure);
+}
+
+export const addMeasure = (data) => {
+    return HttpClient.doPost(api.measure, data);
+}
+export const updateMeasure = (data) => {
+    return HttpClient.doPut(api.measure + "/" + data.id, data);
+}
+export const delMeasure = (data) => {
+    return HttpClient.doDelete(api.measure + "/" + data.id);
+}
